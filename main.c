3cfd57a928d010249a18ea6dd7a6e63b6006a3f9
marlonsalin <salinmengo@gmail.com>
2019-09-18 11:23:31 -0300

    MOVIEMNTO VIA TECLADO

--- main.c
+++ main.c
@@ -10,8 +10,10 @@ int main(){ /// parte principal do programa responsavel por iniciar e chamar as 
     
     char matrix[ROWS][COLUMNS];
     int posI, posJ;
+    int keypressed=0;
 
     //posicao inicial do personagem
+    
     posI = 1;
     posJ = COLUMNS/2;
     //inicializando matriz
@@ -19,16 +21,35 @@ int main(){ /// parte principal do programa responsavel por iniciar e chamar as 
     /// apagar o cursor da tela
            ShowConsoleCursor(0);   
            system("cls");      
-    while(1){
+    while(keypressed != 27){
+        
         gotoxy(0,0);
-
+       
         matrix[posI][posJ] = '@';
 
         printMatrix(matrix);
 
         matrix[posI][posJ] = ' ';
         
-        if(posJ < COLUMNS) posI++;
+        if(posI < (ROWS - 2)) posI++;
+
+         keypressed = 0;
+         if(kbhit()) keypressed = getch();
+         if(keypressed == ARROWS) keypressed = getch();
+        
+            switch(keypressed){
+                case TECLA_AA: 
+                case TECLA_A:
+                case LEFT:
+                 if(posJ > 0) posJ--; /// tecla seta <-
+            break; 
+            case TECLA_DD:
+            case TECLA_D:
+            case RIGHT: if(posJ < COLUMNS - 2) posJ++; /// tecla seta ->
+            break;
+
+            }
+       
     }
 
     system("pause");
--- tetris.h
+++ tetris.h
@@ -5,10 +5,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h> //biblioteca para capturar teclas  
 
 // configuração das dimensões da matriz principal do jogo 
 #define COLUMNS 60
 #define ROWS 25
+
+/// configuração de teclas
+#define ESC 27
+#define ARROWS 224
+#define LEFT 75
+#define RIGHT 77
+#define TECLA_A 97
+#define TECLA_D 100
+#define TECLA_AA 65
+#define TECLA_DD 68
+
 
 /// inicializa a matriz principal com "espaços vazios"
 void init(char matrix[ROWS][COLUMNS]);
